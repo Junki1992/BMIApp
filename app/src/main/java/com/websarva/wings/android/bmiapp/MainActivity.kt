91a2de6477bfe4573a6c8add35000106d196a113
林絢己 <junki.warner@me.com>
2020-08-13 15:10:47 +0900

    計算結果をToastで表示するのをやめて、 TextViewでテキストとして受け取る仕様に変更

--- app/src/main/java/com/websarva/wings/android/bmiapp/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/bmiapp/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.websarva.wings.android.bmiapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.roundToLong
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,17 +24,19 @@ class MainActivity : AppCompatActivity() {
     private inner class CalucButtonListener : View.OnClickListener {
         override fun onClick(view: View) {
             //身長を表示するeditTextオブジェクトを取得
-            val input = findViewById<EditText>(R.id.editText).text.toString().toFloat()
+            val input = findViewById<EditText>(R.id.editHeight).text.toString().toFloat()
             //体重を取得するeditText2オブジェクト取得
-            val input2 = findViewById<EditText>(R.id.editText2).text.toString().toFloat()
+            val input2 = findViewById<EditText>(R.id.editWeight).text.toString().toFloat()
             //身長を変換(cm → m)
             val met = input / 100
             //BMIを計算
             val bmi = input2 / (met * met)
-            val toast = Toast.makeText(applicationContext, "%,.1f".format(bmi), Toast.LENGTH_LONG)
+            //val toast = Toast.makeText(applicationContext, "%,.1f".format(bmi), Toast.LENGTH_LONG)
             // 位置調整
-            toast.setGravity(Gravity.TOP, -350, 1100)
-            toast.show()
+            //toast.setGravity(Gravity.TOP, -350, 1100)
+            //toast.show()
+            //計算結果を表示
+            textViewBMI.text = ("%,.1f".format(bmi))
         }
     }
 }
