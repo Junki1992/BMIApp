266c1af06b7ea68f7602ba435d31dd2adc66a5cc
林絢己 <junki.warner@me.com>
2020-08-14 23:31:26 +0900

    CalcActivityのメソッド構成を整理 BMIの文字色を変更

--- app/src/main/java/com/websarva/wings/android/bmiapp/CalcActivity.kt
+++ app/src/main/java/com/websarva/wings/android/bmiapp/CalcActivity.kt
@@ -1,9 +1,9 @@
 package com.websarva.wings.android.bmiapp
 
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_calc.*
 
 class CalcActivity : AppCompatActivity() {
@@ -14,21 +14,37 @@ class CalcActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calc)
 //        realm = Realm.getDefaultInstance()
-        CalcButton.setOnClickListener {
+        calcButton.setOnClickListener {
+            onCalc()
+        }
+
+        saveButton.setOnClickListener {
             if (editHeight.text.toString() == "" || editWeight.text.toString() == "") {
-                Toast.makeText(applicationContext, "身長と体重を入力してください", Toast.LENGTH_SHORT).show()
-            } else {
-                val inputHeight = editHeight.text.toString().toFloat()
-                val inputWeight = editWeight.text.toString().toFloat()
-                val met = inputHeight / 100
-                val bmi = inputWeight / (met * met)
-                textViewBMI.text = ("%,.1f".format(bmi))
+                Toast.makeText(applicationContext, "計算を完了させて下さい", Toast.LENGTH_SHORT).show()
             }
         }
 
-        ClearButton.setOnClickListener {
+        clearButton.setOnClickListener {
             editHeight.editableText.clear()
             editWeight.editableText.clear()
+            textViewBMI.text = ""
+        }
+
+        backButton.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun onCalc() {
+        if (editHeight.text.toString() == "" || editWeight.text.toString() == "") {
+            Toast.makeText(applicationContext, "身長と体重を入力してください", Toast.LENGTH_SHORT).show()
+        } else {
+            val inputHeight = editHeight.text.toString().toFloat()
+            val inputWeight = editWeight.text.toString().toFloat()
+            val met = inputHeight / 100
+            val bmi = inputWeight / (met * met)
+            textViewBMI.text = ("%,.1f".format(bmi))
+            textViewBMI.setTextColor(Color.BLACK)
         }
     }
 }
